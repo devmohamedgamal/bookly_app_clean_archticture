5fd620117dcc9f225f77a8379320975ad51fea0e
devmohamedgamal <mpedo2010@gmail.com>
2023-10-17 10:54:18 +0300

    first commit

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,8 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String autherName;
+  final num price, rating;
+
+  BookEntity(this.image, this.title, this.autherName, this.price, this.rating);
+}
