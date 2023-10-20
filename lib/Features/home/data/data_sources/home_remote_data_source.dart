b232f2630998aae853487e524b8120c474fd8221
devmohamedgamal <mpedo2010@gmail.com>
2023-10-20 12:06:39 +0300

    impl remote fech featured books

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,21 +1,39 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/utils/api_service.dart';
+
 import '../../domain/entities/book_entity.dart';
 
-abstract class HomeRemoteDataSource{
-  Future <List<BookEntity>> fetchFeaturedBooks();
-  Future <List<BookEntity>> fetchNewestBooks();
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
 
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
 
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+  HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+        endPoint: "volumes?Filtering=free-ebooks&q=programming");
+    List<BookEntity> books = getBooksList(data);
+    return books;
   }
-
+  
   @override
   Future<List<BookEntity>> fetchNewestBooks() {
     // TODO: implement fetchNewestBooks
     throw UnimplementedError();
   }
+
+
+
+   List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
+  }
+
 }
